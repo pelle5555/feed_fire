9af1e06b45094349f14546bf1fdeee8150d0907d
Niklas Pelle <nicolaum1609@gmail.com>
2026-02-10 21:25:55 +0100

    added fire and tree sprites

---
+++ Scripts/Inventory.gd
@@ -0,0 +1,27 @@
+extends MarginContainer
+
+@onready var wood_label: Label = $HBoxContainer/WoodLabel
+
+@export var starting_wood: int = 1
+
+var wood_amount: int
+
+func _ready() -> void:
+	wood_amount = starting_wood
+	wood_label.text = str(starting_wood)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		add_wood()
+
+func add_wood() -> void:
+	print("wood added")
+	wood_amount += 1
+	wood_label.text = str(wood_amount)
+	
+func subtract_wood() -> void:
+	print("wood subtracted")
+	wood_amount -= 1
+	wood_label.text = str(wood_amount)
+	
+	
---
+++ Scripts/fire.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var inventory: MarginContainer = $"../Inventory"
+
+
+@export var fire_starting_size: float = 1
+@export var fire_increase: float = 0.1
+var fire_size: float
+
+func _ready() -> void:
+	fire_size = fire_starting_size
+
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if inventory.wood_amount > 0:
+				add_to_fire()
+			else:
+				print("not enough wood")
+		
+func add_to_fire() -> void:
+	print("added wood to fire")
+	fire_size += fire_increase
+	animated_sprite_2d.scale = Vector2(fire_size, fire_size)
+	inventory.subtract_wood()
