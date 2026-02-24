cfd025207fc76214ae1362bcb4efaf5b98ab8bde
Niklas Pelle <nicolaum1609@gmail.com>
2026-02-24 20:09:39 +0100

    added incluence on embers

--- Scripts/mouse_input_component.gd
+++ Scripts/mouse_input_component.gd
@@ -1,18 +1,43 @@
 extends Area2D
-@export var clickable: Node2D
 
-var _influence: Node = null
+@onready var timer: Timer = $Timer
+
+@export var ember_scene: PackedScene
+@export var clickable: Node2D
 	
-func _get_influence() -> Node:
-	if _influence == null:
-		_influence = get_tree().get_first_node_in_group("influence")
-	return _influence
+var _influence: Array
+var ember_position: Vector2
+
+func get_influence() -> Array:
+	return get_tree().get_nodes_in_group("influence")
+	
+func is_position_in_any_influence(pos: Vector2) -> bool:
+	for influence in get_influence():
+		if influence.is_position_reachable(pos):
+			return true
+	return false
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var influence := _get_influence()
-			
-			if influence != null and influence.is_position_reachable(clickable.global_position): 
+			if is_position_in_any_influence(clickable.global_position): 
 				if clickable != null and clickable.has_method("mouse_click"):
 					clickable.mouse_click()
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+			timer.start()
+			ember_position = clickable.global_position
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			print("pressing")
+		elif event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+			print("releasing")
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			timer.stop()
+
+func create_ember() -> void:
+	var ember = ember_scene.instantiate()
+	add_child(ember)
+	ember.global_position = ember_position
+
+func _on_timer_timeout() -> void:
+	print("build ember")
+	create_ember()
